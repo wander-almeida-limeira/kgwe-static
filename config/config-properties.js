ff26f953b32796a1ebe9582b1bb84f24e030d38c
Wander <wander.almeida.limeira@gmail.com>
2020-11-17 12:39:45 -0300

    Config

--- config/config-properties.js
+++ config/config-properties.js
@@ -1,6 +1,6 @@
 configProperties = {
     // Este arquivo possui todos as propriedades de configura��o da aplica��o
-    apiURL: 'http://ec2-user@ec2-3-90-104-149.compute-1.amazonaws.com:8080/',
+    apiURL: 'http://java.icmc.usp.br:2680/',
     //apiURL: 'http://localhost:8080/',
     sustenagro: 'http://3.224.120.121/',
     mailto: 'dilvan@icmc.usp.br',
